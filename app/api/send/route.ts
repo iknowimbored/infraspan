e6ada3eaeed0d0689272e79fa2087ba0cc45c362
iknowimbored <kayle@infraspan.com.au>
2025-05-12 18:39:42 +0800

    feat: 🎸 admin email template

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,4 +1,5 @@
 import { ContactUsInputs } from "@/lib/@types/contact";
+import { AdminEmailTemplate } from "@/lib/components/admin-email-template";
 import { EmailTemplate } from "@/lib/components/email-template";
 import { NextRequest, NextResponse } from "next/server";
 import { ReactNode } from "react";
@@ -11,11 +12,21 @@ export async function POST(request: NextRequest) {
     const body: ContactUsInputs = await request.json();
     const { error } = await resend.emails.send({
       from: "Infraspan Info <info@infraspan.com.au>",
-      to: [body.email, "info@infraspan.com.au"],
+      to: ["info@infraspan.com.au"],
+      subject:
+        "New Contact Form Submission from " + body.companyName.toUpperCase(),
+      react: AdminEmailTemplate(body) as ReactNode,
+    });
+    if (error) {
+      throw new Error();
+    }
+    const { error: error1 } = await resend.emails.send({
+      from: "Infraspan Info <info@infraspan.com.au>",
+      to: [body.email],
       subject: "Thank You for Contacting Infraspan",
       react: EmailTemplate(body) as ReactNode,
     });
-    if (error) {
+    if (error1) {
       throw new Error();
     }
     return NextResponse.json({ success: true });
---
+++ lib/components/admin-email-template.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { ContactUsInputs } from "../@types/contact";
+
+export const AdminEmailTemplate: React.FC<Readonly<ContactUsInputs>> = (
+  props
+) => (
+  <div>
+    <h2>Hi, Admin!</h2>
+    <p>
+      You have received a new message through the contact form on
+      <a href="https://www.infraspan.com.au">www.infraspan.com.au</a>.
+    </p>
+    <p>Here are the details of the submission:</p>
+    <dl>
+      <dt style={{ fontWeight: "bold" }}>Full Name</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.fullName}</dd>
+      <dt style={{ fontWeight: "bold" }}>Email Address</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.email}</dd>
+      <dt style={{ fontWeight: "bold" }}>Phone Numner</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.phoneNumber}</dd>
+      <dt style={{ fontWeight: "bold" }}>Company Name</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.companyName}</dd>
+      <dt style={{ fontWeight: "bold" }}>Message</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.message}</dd>
+    </dl>
+    <p>
+      Sincerely, <br /> <strong>Infraspan System Notifications</strong>
+    </p>
+  </div>
+);
