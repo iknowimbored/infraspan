df506f2adad73fab0b911df9902fa234e4c23dff
iknowimbored <kayle@infraspan.com.au>
2025-05-12 17:52:27 +0800

    fix: 🐛 email template typo

--- lib/components/email-template.tsx
+++ lib/components/email-template.tsx
@@ -5,7 +5,7 @@ export const EmailTemplate: React.FC<Readonly<ContactUsInputs>> = (props) => (
   <div>
     <h2>Hi, {props.fullName}!</h2>
     <p>
-      Thank you for contactis us at{" "}
+      Thank you for contacting us at{" "}
       <a href="https://www.infraspan.com.au">www.infraspan.com.au</a>.
     </p>
     <p>
@@ -16,16 +16,16 @@ export const EmailTemplate: React.FC<Readonly<ContactUsInputs>> = (props) => (
       For your reference, here's a summary of the information you submitted:
     </p>
     <dl>
-      <dt>Full Name</dt>
-      <dd>{props.fullName}</dd>
-      <dt>Email Address</dt>
-      <dd>{props.email}</dd>
-      <dt>Phone Numner</dt>
-      <dd>{props.phoneNumber}</dd>
-      <dt>Company Name</dt>
-      <dd>{props.companyName}</dd>
-      <dt>Message</dt>
-      <dd>{props.message}</dd>
+      <dt style={{ fontWeight: "bold" }}>Full Name</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.fullName}</dd>
+      <dt style={{ fontWeight: "bold" }}>Email Address</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.email}</dd>
+      <dt style={{ fontWeight: "bold" }}>Phone Numner</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.phoneNumber}</dd>
+      <dt style={{ fontWeight: "bold" }}>Company Name</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.companyName}</dd>
+      <dt style={{ fontWeight: "bold" }}>Message</dt>
+      <dd style={{ margin: "0 0 1rem 0" }}>{props.message}</dd>
     </dl>
     <p>
       Sincerely, <br /> <strong>The Infraspan Team</strong>
